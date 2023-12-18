2577e5c5c668d6c1067933603227dd0ce617944f
Lance-Drane <ldraneutk@gmail.com>
2023-12-17 22:57:49 -0500

    2015 puzzle 24 solution
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

---
+++ 2015/24/2.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import combinations
+from math import prod
+
+presents = [int(line) for line in sys.stdin.readlines()]
+target = sum(presents) >> 2
+
+combo_length = 1
+combos = []
+while True:
+    combos = list(filter(lambda c: sum(c) == target, combinations(presents, combo_length)))
+    if combos:
+        break
+    combo_length += 1
+
+print(min(prod(combo) for combo in combos))
