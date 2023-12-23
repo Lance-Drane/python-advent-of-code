5ce6504832897f2ef0105c8a8b5b73977884e805
Lance-Drane <ldraneutk@gmail.com>
2023-12-22 21:24:45 -0500

    2015 day 19 - skip a data conversion
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

--- 2015/19/2.py
+++ 2015/19/2.py
@@ -8,11 +8,11 @@ for line in lines[:-2]:
     graph[end] = start
 string = lines[-1].rstrip()
 
-graph = dict(sorted(graph.items(), reverse=True, key=lambda i: len(i[0])))
+graph = sorted(graph.items(), reverse=True, key=lambda i: len(i[0]))
 count = 0
 
 while string != 'e':
-    for end, start in graph.items():
+    for end, start in graph:
         count += string.count(end)
         string = string.replace(end, start)
 
