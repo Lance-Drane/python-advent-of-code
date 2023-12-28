10b2a679a78b22c3df4995cb2b5d286f9fa02940
Lance-Drane <ldraneutk@gmail.com>
2023-12-28 02:10:29 -0500

    2016 day 14 solution
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

---
+++ 2016/14/2.py
@@ -0,0 +1,34 @@
+import re
+from hashlib import md5
+from itertools import count
+
+
+def run():
+    key = input()
+
+    hashes = {}
+    counter = 0
+    break_point = float('inf')
+
+    for number in count(start=0):
+        if number > break_point:
+            return [key for key, value in hashes.items() if value is True][63]
+        hsh = md5(f'{key}{number}'.encode()).hexdigest()
+        for _ in range(2016):
+            hsh = md5(hsh.encode()).hexdigest()
+        first_triplet = re.search(r'(.)\1{2}', hsh)
+        if first_triplet:
+            old_hash_keys = [[key, value] for key, value in hashes.items() if key >= number - 1000]
+            hashes[number] = first_triplet.group(1)
+            for quintuplet in {q.group(1) for q in re.finditer(r'(.)\1{4}', hsh)}:
+                for items in filter(lambda i: i[1] == quintuplet, old_hash_keys):
+                    hashes[items[0]] = True
+                    counter += 1
+                    if counter == 64:
+                        break_point = items[0] + 1000
+
+    # unreachable
+    return None
+
+
+print(run())
