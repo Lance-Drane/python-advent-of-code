776425b6ced08e37871de3d7fd2ba203b33735f0
Lance-Drane <ldraneutk@gmail.com>
2024-01-04 21:39:35 -0500

    2016 day 16 solutions
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

---
+++ 2016/16/2.py
@@ -0,0 +1,17 @@
+def flip_bits(binary_str: str):
+    inverse = int(binary_str, 2) ^ (2 ** (len(binary_str) + 1) - 1)
+    return bin(inverse)[3:]
+
+
+TARGET_LENGTH = 35651584
+
+data = input()
+while len(data) < TARGET_LENGTH:
+    data = f'{data}0{flip_bits(data[::-1])}'
+data = data[:TARGET_LENGTH]
+
+while len(data) & 1 != 1:
+    # in python 3.12, can potentially use itertools.batched instead
+    data = ['1' if chunk[0] == chunk[1] else '0' for chunk in zip(*[iter(data)] * 2, strict=False)]
+
+print(''.join(data))
