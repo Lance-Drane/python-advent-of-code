bf908babeba1b98371d3594ca27306e5c6c89290
Lance-Drane <ldraneutk@gmail.com>
2024-01-14 14:03:53 -0500

    2017 day 8 solution
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

---
+++ 2017/08/1.py
@@ -0,0 +1,25 @@
+import operator
+import sys
+from collections import defaultdict
+
+registers: defaultdict[str, int] = defaultdict(lambda: 0)
+operations = {
+    '>': operator.gt,
+    '<': operator.lt,
+    '>=': operator.ge,
+    '<=': operator.le,
+    '==': operator.eq,
+    '!=': operator.ne,
+}
+
+for line in sys.stdin.readlines():
+    parts = line.split()
+    register_check_val = registers[parts[4]]
+    check_val = int(parts[6])
+    if operations[parts[5]](register_check_val, check_val):
+        if parts[1] == 'inc':
+            registers[parts[0]] += int(parts[2])
+        else:
+            registers[parts[0]] -= int(parts[2])
+
+print(max(registers.values()))
---
+++ 2017/08/2.py
@@ -0,0 +1,27 @@
+import operator
+import sys
+from collections import defaultdict
+
+registers: defaultdict[str, int] = defaultdict(lambda: 0)
+operations = {
+    '>': operator.gt,
+    '<': operator.lt,
+    '>=': operator.ge,
+    '<=': operator.le,
+    '==': operator.eq,
+    '!=': operator.ne,
+}
+
+highest = 0
+for line in sys.stdin.readlines():
+    parts = line.split()
+    register_check_val = registers[parts[4]]
+    check_val = int(parts[6])
+    if operations[parts[5]](register_check_val, check_val):
+        if parts[1] == 'inc':
+            registers[parts[0]] += int(parts[2])
+        else:
+            registers[parts[0]] -= int(parts[2])
+        highest = max(highest, registers[parts[0]])
+
+print(highest)
